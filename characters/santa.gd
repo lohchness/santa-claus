684af7f071d4c689c0c05f69baca1734f366cf69
Loki Chow <lohchness@gmail.com>
2025-07-05 15:51:45 +1000

    feat: movement delay for one reindeer

--- characters/santa.gd
+++ characters/santa.gd
@@ -2,11 +2,28 @@ extends CharacterBody2D
 
 @export var reindeer_array: Array[CollisionShape2D] = []
 
-var reindeer_delay_ms: int = 250
+# VERY BAD IDEA! 
+var mouse_points: Array[float] = []
+var delay_ms: float = .25
+const FRAMES: int = 60
 
+func _ready() -> void:
+	mouse_points = []
 
 func _physics_process(delta: float) -> void:
 	var mouseY = get_global_mouse_position().y
+	add_mouse_y(mouseY)
+	
+	if len(mouse_points) < 15:
+		return
+	
+	var delayed_position = pop_mouse_y()
 	
 	for i in reindeer_array:
-		i.position.y = mouseY
+		i.position.y = delayed_position
+
+func add_mouse_y(p: float):
+	mouse_points.append(p)
+
+func pop_mouse_y():
+	return mouse_points.pop_front()
