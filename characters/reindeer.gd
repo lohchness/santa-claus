0940330aacad9a95530de7acf1cfc3a5fcc36af2
Loki Chow <lohchness@gmail.com>
2025-07-05 16:15:46 +1000

    feat: store history for each reindeer, decrease delay. much smoother movement

--- characters/reindeer.gd
+++ characters/reindeer.gd
@@ -1,1 +1,6 @@
 extends CollisionShape2D
+
+var position_history: Array[float] = []
+
+func _physics_process(delta: float) -> void:
+	position_history.append(global_position.y)
--- characters/santa.gd
+++ characters/santa.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 
 # VERY BAD IDEA! 
 var mouse_points: Array[float] = []
-var delay_ms: float = .25
+var delay_ms: float = .08
 const FRAMES: int = 60
 
 func _ready() -> void:
@@ -12,18 +12,22 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	var mouseY = get_global_mouse_position().y
-	add_mouse_y(mouseY)
+	mouse_points.append(mouseY)
 	
-	if len(mouse_points) < 15:
+	if len(mouse_points) < int(FRAMES*delay_ms):
 		return
 	
-	var delayed_position = pop_mouse_y()
+	var delayed_position = mouse_points.pop_front()
 	
-	#for i in reindeer_array:
-		#i.position.y = delayed_position
-	reindeer_array[0].position.y = lerp(reindeer_array[0].position.y, mouseY, .1)
+	reindeer_array[0].position.y = lerp(reindeer_array[0].position.y, delayed_position, .3)
 	for i in range(1, len(reindeer_array)):
-		reindeer_array[i].position.y = lerp(reindeer_array[i].position.y, reindeer_array[i-1].position.y, .1)
+		#reindeer_array[i].position.y = reindeer_array[i-1].position_history.pop_front()
+		reindeer_array[i].position.y = lerp(reindeer_array[i].position.y, reindeer_array[i-1].position_history.pop_front(), .3)
+	
+	
+	#reindeer_array[0].position.y = lerp(reindeer_array[0].position.y, mouseY, .2)
+	#for i in range(1, len(reindeer_array)):
+		#reindeer_array[i].position.y = lerp(reindeer_array[i].position.y, reindeer_array[i-1].position.y, .4)
 
 func add_mouse_y(p: float):
 	mouse_points.append(p)
