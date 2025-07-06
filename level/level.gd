16195bdcd30bf8f5b5401cae25d8e25781cf9751
Loki Chow <lohchness@gmail.com>
2025-07-06 22:14:57 +1000

    feat: add balloon rising from floor

--- level/level.gd
+++ level/level.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var missile: PackedScene = preload("res://obstacles/rocket/horizontal_rocket.tscn")
+@onready var balloon: PackedScene = preload("res://obstacles/balloon/balloon.tscn")
 
 func _physics_process(delta: float) -> void:
 	pass
@@ -10,3 +11,10 @@ func _on_missile_timer_timeout() -> void:
 	var m = missile.instantiate()
 	add_child(m)
 	m.global_position = Vector2(1300, randi_range(50, 600))
+
+
+
+func _on_balloon_timer_timeout() -> void:
+	var b = balloon.instantiate()
+	add_child(b)
+	b.global_position = Vector2(randi_range(100, 1100), 800)
---
+++ obstacles/balloon/balloon.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+const SPEED = 500
+
+func _physics_process(delta: float) -> void:
+	position += Vector2(-100, -SPEED) * delta
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.has_method("receive_damage"):
+		body.receive_damage()
+	queue_free()
