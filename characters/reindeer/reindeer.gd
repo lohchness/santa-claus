600926a8efef15d3a843b6f4ce7547c101fac70c
Loki Chow <lohchness@gmail.com>
2025-07-06 20:51:36 +1000

    feat: reindeer sprite animated

--- characters/reindeer/reindeer.gd
+++ characters/reindeer/reindeer.gd
@@ -2,5 +2,9 @@ extends CollisionShape2D
 
 var position_history: Array[float] = []
 
+func _ready() -> void:
+	if randi_range(0, 1):
+		$AnimatedSprite2D.frame = 6
+
 func _physics_process(delta: float) -> void:
 	position_history.append(global_position.y)
