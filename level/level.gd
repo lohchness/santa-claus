498ec3b2069d25dd01452a76ccdfeeec35ebaa42
Loki Chow <lohchness@gmail.com>
2025-07-08 22:26:19 +1000

    feat: timings

--- level/level.gd
+++ level/level.gd
@@ -10,13 +10,15 @@ extends Node2D
 var t = 0
 var score = 0
 
+func _ready() -> void:
+	santa.connect("dead", Callable(self,"end_game"))
+
 func _physics_process(delta: float) -> void:
 	t += delta
 	score = int(t * 10)
 	
 	label.text = str(score)
-	
-	santa.connect("dead", Callable(self,"end_game"))
+
 
 func end_game():
 	Globals.score = score
@@ -26,16 +28,36 @@ func _on_missile_timer_timeout() -> void:
 	var m = missile.instantiate()
 	add_child(m)
 	m.global_position = Vector2(1300, randi_range(50, 600))
-
+	
+	$MissileTimer.wait_time = randf_range(0.6, 1.6)
 
 
 func _on_balloon_timer_timeout() -> void:
 	var b = balloon.instantiate()
 	add_child(b)
 	b.global_position = Vector2(randi_range(100, 1100), 800)
+	
+	$BalloonTimer.wait_time = randf_range(2, 8)
 
 
 func _on_jet_timer_timeout() -> void:
 	var j = jet.instantiate()
 	add_child(j)
 	j.global_position = Vector2(-175, randi_range(20, 90))
+	
+	if randi_range(0, 3) == 0:
+		var j2 = jet.instantiate()
+		add_child(j2)
+		j2.global_position = Vector2(-175, randi_range(20, 90))
+	
+	$JetTimer.wait_time = randf_range(3, 6)
+
+
+func _on_start_grace_period_timeout() -> void:
+	$MissileTimer.start()
+
+func _on_jet_grace_period_timeout() -> void:
+	$JetTimer.start()
+
+func _on_balloon_grace_period_timeout() -> void:
+	$BalloonTimer.start()
