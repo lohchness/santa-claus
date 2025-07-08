aa25dcdfa3d95516f352761a21ce61203f168bb1
Loki Chow <lohchness@gmail.com>
2025-07-08 19:28:22 +1000

    refactor: change horizontal rocket to snowball

--- obstacles/rocket/horizontal_rocket.gd
+++ obstacles/rocket/horizontal_rocket.gd
@@ -1,13 +1,20 @@
-extends Area2D
+extends CharacterBody2D
 
-const SPEED = 1500
+var speed
+var jump_velocity
+
+func _ready() -> void:
+	speed = randi_range(-500, -800)
+	jump_velocity = randi_range(-400, -700)
+	velocity.y = jump_velocity
 
 func _physics_process(delta: float) -> void:
-	position += Vector2(-SPEED, 0) * delta
+	velocity.x = speed
+	velocity += Vector2(0, 700) * delta
+	move_and_slide()
 
 
-func _on_body_entered(body: Node2D) -> void:
-	print("hello")
+func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.has_method("receive_damage"):
 		body.receive_damage()
 	queue_free()
